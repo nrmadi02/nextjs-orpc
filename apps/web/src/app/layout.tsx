82d8828cefccc47a709a36d229d8a24a54da2073
nrmadi02 <ahmadinur2202@gmail.com>
2025-08-02 14:23:38 +0800

    feat: initial setup prisma

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <div className="grid grid-rows-[auto_1fr] h-svh">
+          <div className="grid h-svh grid-rows-[auto_1fr]">
             <Header />
             {children}
           </div>
